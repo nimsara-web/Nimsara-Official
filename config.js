2159a1532c9dd8c470b220ce226090483b6c9ad6
Nimsara <nimsaranethmintha@gmail.com>
2026-09-24 00:48:59 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  botMini: "https://nim-bot-mini.onrender.com/",
+  botNimora: "https://nimora-md-v2.onrender.com/",
+  botMiniApi: "https://nim-bot-mini.onrender.com/pair",
+  botNimoraApi: "https://nimora-md-v2.onrender.com/pair",
+  contactNumber: "94784280074",
+  contactMsg: "Hy nim",
+  startTimeKey: "nimora_site_start"
+};
